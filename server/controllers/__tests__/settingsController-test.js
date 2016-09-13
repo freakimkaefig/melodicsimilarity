41e95169d91e975e9b571ff987d68dec00fb67f2
freakimkaefig <lukas.lamm89@gmail.com>
2016-09-13 23:23:58 +0200

    Add test for settingsController

---
+++ server/controllers/__tests__/settingsController-test.js
@@ -0,0 +1,60 @@
+import request from 'supertest';
+
+describe('settingsController', () => {
+
+  var server;
+  var auth;
+
+  beforeEach(() => {
+    server = require('../../test/server-helper');
+    auth = require('../../test/auth-helper');
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+
+  it('should get setting', (done) => {
+    request(server)
+      .get('/api/settings/threshold')
+      .end((err, res) => {
+        expect(err).toEqual(null);
+        expect(res.status).toBe(200);
+        expect(res.headers['content-type']).toMatch(/json/);
+        expect(res.body.type).toBeDefined();
+        expect(res.body.key).toBeDefined();
+        expect(res.body.value).toBeDefined();
+        done();
+      });
+  });
+
+  it('should disable set setting when unauthorized', (done) => {
+    request(server)
+      .put('/api/protected/settings/threshold/20')
+      .end((err, res) => {
+        expect(err).toEqual(null);
+        expect(res.status).toBe(401);
+        done();
+      });
+  });
+
+  it('should allow set setting when authorized', (done) => {
+    return auth.authenticate(server)
+      .then((token) => {
+        request(server)
+          .put('/api/protected/settings/threshold/20')
+          .set('Authorization', 'Bearer ' + token)
+          .end((err, res) => {
+            expect(err).toEqual(null);
+            expect(res.status).toBe(200);
+            expect(res.body.ok).toBe(1);
+            expect(res.body.value).toBeDefined();
+            expect(res.body.value.type).toBeDefined();
+            expect(res.body.value.key).toBeDefined();
+            expect(res.body.value.value).toBeDefined();
+            done();
+          });
+      });
+  });
+
+});
