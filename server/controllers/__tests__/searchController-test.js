e24a6e2f371c890eb7073bbbe6bea75bea600d95
freakimkaefig <lukas.lamm89@gmail.com>
2016-09-13 23:23:50 +0200

    Add test for searchController

---
+++ server/controllers/__tests__/searchController-test.js
@@ -0,0 +1,117 @@
+import request from 'supertest';
+
+describe('searchController', () => {
+
+  var server;
+
+  beforeEach(() => {
+    server = require('../../test/server-helper');
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+
+  it('should return solr results for "liebe"', (done) => {
+    request(server)
+      .post('/api/search')
+      .send({
+        "solrQuery": "http://127.0.0.1:8983/solr/searchableDocs/query?wt=json&hl=true&hl.simple.pre=<em>&hl.simple.post=</em>&hl.fragsize=300&hl.snippets=3&q=liebe&fq=signature:*",
+        "melodyMode": "MELODY",
+        "melodyQuery": false
+      })
+      .end((err, res) => {
+        expect(err).toEqual(null);
+        expect(res.status).toBe(200);
+        expect(res.headers['content-type']).toMatch(/json/);
+        expect(res.body.length).toBeGreaterThan(0);
+        done();
+      });
+  });
+
+  it('should return melody results for "*udr"', (done) => {
+    request(server)
+      .post('/api/search')
+      .send({
+        "solrQuery": false,
+        "melodyMode": "PARSONS",
+        "melodyQuery": {
+          "parson": "udr",
+          "threshold": 75
+        }
+      })
+      .end((err, res) => {
+        expect(err).toEqual(null);
+        expect(res.status).toBe(200);
+        expect(res.headers['content-type']).toMatch(/json/);
+        expect(res.body.length).toBeGreaterThan(0);
+        done();
+      });
+  });
+
+  it('should return melody results for "* 2 -4 0"', (done) => {
+    request(server)
+      .post('/api/search')
+      .send({
+        "solrQuery": false,
+        "melodyMode": "INTERVALS",
+        "melodyQuery": {
+          "intervals": "2 -4 0",
+          "threshold": 75
+        }
+      })
+      .end((err, res) => {
+        expect(err).toEqual(null);
+        expect(res.status).toBe(200);
+        expect(res.headers['content-type']).toMatch(/json/);
+        expect(res.body.length).toBeGreaterThan(0);
+        done();
+      });
+  });
+
+  it('should return melody results for "C D A A"', (done) => {
+    request(server)
+      .post('/api/search')
+      .send({
+        "solrQuery": false,
+        "melodyMode": "MELODY",
+        "melodyQuery": {
+          "melody": [
+            { "pitch": { "step": "C", "octave": 4, "accidental": "" }, "rest": false, "duration": 8, "type": "eighth", "dot": false },
+            { "pitch": { "step": "D", "octave": 4, "accidental": "" }, "rest": false, "duration": 8, "type": 'eighth', "dot": false },
+            { "pitch": { "step": "A", "octave": 3, "alter": 1, "accidental": "sharp" }, "rest": false, "duration": 16, "type": "quarter", "dot": false },
+            { "pitch": { "step": "A", "octave": 3, "alter": 1, "accidental": "sharp" }, "rest": false, "duration": 8, "type": "eighth", "dot": false }
+          ],
+          "threshold": 20
+        }
+      })
+      .end((err, res) => {
+        expect(err).toEqual(null);
+        expect(res.status).toBe(200);
+        expect(res.headers['content-type']).toMatch(/json/);
+        expect(res.body.length).toBeGreaterThanOrEqual(0);
+        done();
+      });
+  });
+
+  it('should return combined results', (done) => {
+    request(server)
+      .post('/api/search')
+      .send({
+        "solrQuery": "http://127.0.0.1:8983/solr/searchableDocs/query?wt=json&hl=true&hl.simple.pre=<em>&hl.simple.post=</em>&hl.fragsize=300&hl.snippets=3&q=liebe&fq=signature:*",
+        "melodyMode": "PARSONS",
+        "melodyQuery": {
+          "parson": "udr",
+          "threshold": 10
+        }
+      })
+      .end((err, res) => {
+        expect(err).toEqual(null);
+        expect(res.status).toBe(200);
+        expect(res.headers['content-type']).toMatch(/json/);
+        expect(res.body.length).toBeGreaterThan(0);
+        done();
+      });
+  });
+
+});
